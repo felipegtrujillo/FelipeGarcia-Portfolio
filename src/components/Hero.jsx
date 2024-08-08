c1a76fbef07147aa3326c57a2eac64a3dd2d250b
felipegtrujillo <felipegtrujillo@gmail.com>
2024-08-08 01:09:45 -0400

    text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
          </div>
          <div>
     <motion.h1
-      className={`${styles.heroHeadText} text-[#d56433]`}
+      className={`${styles.heroHeadText} text-[#d36333]`}
       initial={{ opacity: 0, y: -50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 1, ease: 'easeInOut' }}
